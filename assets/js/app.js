03b045cd94ca4602dd70c3f99c618b47bd62da6e
Ahmad Hameed <ahmed.hameed@nkutechnologies.com>
2024-09-04 00:24:44 +0500

    Boilerplate code
    homepage, about us page, contact us page boilerplate code
    header and footer code in separate files
    renderHTML through js to remove code duplication for header/footer
    certain css styling classes added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,18 @@
-console.log("Hello Movers");
+console.log("Movers");
 AOS.init();
+
+const renderHTML = async (elementId, filePath) => {
+  try {
+    const response = await fetch(filePath);
+    if (!response.ok) throw new Error("Network response was not ok");
+    const data = await response.text();
+    document.getElementById(elementId).innerHTML = data;
+  } catch (error) {
+    console.error("Error loading HTML:", error);
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderHTML("header", "../header.html");
+  renderHTML("footer", "../footer.html");
+});
